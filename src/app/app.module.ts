484f08c2614082bc8c1552930dad15367b99af97
Vamsi <vamsi.skrishna@gmail.com>
2018-04-14 08:17:45 +0530

    Added basic page with materialize

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { appRoutes } from './app.routes';
+import { LocationsComponent } from './locations/locations.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LocationsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app.routes.ts
@@ -0,0 +1,10 @@
+import { Routes } from '@angular/router';
+import { LocationsComponent } from './locations/locations.component';
+
+
+export const appRoutes: Routes = [
+  {
+    path: '',
+    component: LocationsComponent
+  },
+];
